bb749d728e44fc3fdd8e6bdc55e8efd38c7c00fb
Gabester0 <gceipper@gmail.com>
2021-01-22 00:37:28 -0500

    initial basic analysis of data

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,13 @@ const matches = fs
     .split('\n')
     .map((row: string): string[] => {
         return row.split(',')
-    })
+    });
 
-console.log(matches)
+let manUnitedWins = 0;
+
+for( let match of matches){
+    if(match[1] === 'Man United' && match[5] === 'H') manUnitedWins++;
+    else if(match[2] === 'Man United' && match[5] === 'A') manUnitedWins++
+}
+
+console.log(`Man United won ${manUnitedWins} games`)
