a36ed93436ad60ee8eadebd3a230234aa24aba7f
Gabester0 <gceipper@gmail.com>
2021-01-22 00:43:26 -0500

    exploring a less than ideal solution to improving our hard-coded values

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,15 @@ const matches = fs
         return row.split(',')
     });
 
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
 let manUnitedWins = 0;
 
 for( let match of matches){
-    if(match[1] === 'Man United' && match[5] === 'H') manUnitedWins++;
-    else if(match[2] === 'Man United' && match[5] === 'A') manUnitedWins++
+    if(match[1] === 'Man United' && match[5] === homeWin) manUnitedWins++;
+    else if(match[2] === 'Man United' && match[5] === awayWin) manUnitedWins++
 }
 
 console.log(`Man United won ${manUnitedWins} games`)
